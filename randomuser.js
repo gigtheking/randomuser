d9aaf8c5de2379a9793d6a8d2210eda6c5ac32a3
gideonkings <gsimon931@gmail.com>
2021-10-01 11:34:20 +0100

    updated the component style

--- randomuser.js
+++ randomuser.js
@@ -8,16 +8,16 @@ fetch("https://randomuser.me/api/?results=10")
 
 
         //get data value
-        let output = "<h2><center>Get User Data</center></h2>";
+        let output = "<h2><center>Get Random User Data</center></h2>";
 
         //get data loop through
         author.forEach(function(lists){
             output +=`
             
-                <div class="card mb-4" style="display:flex; background-color:black;">
-                    <div style="background-color:green; width:20%; float:left"><img class="" style="height:300px; width:100%;" src="${lists.picture.medium}"></div>
-                    <div class=" w-75" style="background-color:red;">
-                        <h2 class="card-title m-2">Name: ${lists.name.title} ${lists.name.first} ${lists.name.last} <span> (${lists.dob.age})</span></h2>
+                <div class="card">
+                    <img class="card-img"  src="${lists.picture.medium}">
+                    <div class="card-box">
+                        <h2 class="card-title ">Name: ${lists.name.title} ${lists.name.first} ${lists.name.last} <span> (${lists.dob.age})</span></h2>
                         <strong>Gender: ${lists.gender}</strong><br>
                         <span>Address: ${lists.location.street.number}, ${lists.location.street.name}, ${lists.location.city}, </span><br>
                         <small>State: ${lists.location.state}</small><br>
